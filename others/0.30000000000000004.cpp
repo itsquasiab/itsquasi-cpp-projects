ab7063fa519430c7864bd9c0e3ba62666847344f
ItsQuasi <truequasi2010@gmail.com>
2026-02-13 17:43:44 +0700

    Basic setup changes

--- others/0.30000000000000004.cpp
+++ others/0.30000000000000004.cpp
@@ -1,19 +1,10 @@
-//Chasing the goal...
-//MΔDE BY ITSQUΔSI
-#include <bits/stdc++.h>
-#define ll long long
-#define ld long double
+#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    /*if (fopen(".inp","r"))
-    {
-        freopen(".inp", "r", stdin);
-        freopen(".out", "w", stdout);
-    }*/
-    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
     cout << setprecision(17) << 0.1 + 0.2;
     return 0;
 }
---
+++ others/cpp_version.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    if (__cplusplus == 202302L) cout << "C++23";
+    else if (__cplusplus == 202002L) cout << "C++20";
+    else if (__cplusplus == 201703L) cout << "C++17";
+    else if (__cplusplus == 201402L) cout << "C++14";
+    else if (__cplusplus == 201103L) cout << "C++11";
+    else if (__cplusplus == 199711L) cout << "C++98";
+    else cout << "pre-standard C++.";
+    cout << " ("__cplusplus")\n";
+}
