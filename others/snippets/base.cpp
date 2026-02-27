a10f5c5c8a19b2e1a881e98f20c593497e8a436d
ItsQuasi <truequasi2010@gmail.com>
2026-02-27 19:21:02 +0700

    Updating snippet one more time

--- others/snippets/base.cpp
+++ others/snippets/base.cpp
@@ -2,20 +2,20 @@
 // MADE BY ITSQUASI
 #include <iostream>
 #define ll long long
-// #define task ""
+#define task "base"
 
 using namespace std;
 
-const ll arr = 1'000'006, mod = 1'000'000'007;
+const ll arr = 1e6 + 6, mod = 1e9 + 7;
 
 int main()
 {
     ios::sync_with_stdio(0), cin.tie(0);
-    /* if (fopen(task ".inp", "r"))
+    if (fopen(task ".inp", "r"))
     {
         freopen(task ".inp", "r", stdin);
         freopen(task ".out", "w", stdout);
-    } */
+    }
     
     return 0;
 }
